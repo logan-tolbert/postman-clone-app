91b131e9c0ded9285cdff25084e41971fb47a0fc
logan-Tolbert <lodev205@gmail.com>
2024-12-12 20:53:55 -0600

    :construction: Update CallApiAsync logic

--- PostmanCloneLibrary/ApiAccess.cs
+++ PostmanCloneLibrary/ApiAccess.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace PostmanCloneLibrary
@@ -10,18 +11,31 @@ namespace PostmanCloneLibrary
     {
         private readonly HttpClient _httpClient = new();
 
-        public async Task<string> CallApiAsync(string url)
+        public async Task<string> CallApiAsync(string url, bool normalizeOutput = true)
         {
           var response = await _httpClient.GetAsync(url);
 
             if (response.IsSuccessStatusCode)
             {
                 string json = await response.Content.ReadAsStringAsync();
-                return json;
+
+                if (normalizeOutput)
+                {
+                    var jsonElement = JsonSerializer.Deserialize<JsonElement>(json);
+
+                    string normalizedJson = JsonSerializer.Serialize(jsonElement, 
+                        new JsonSerializerOptions { WriteIndented=true });
+
+                    return normalizedJson;
+                }    
+                else
+                {
+                    return json;
+                }
             }
             else
             {
-                return$"Error: {response.StatusCode}";
+                return $"Error: {response.StatusCode}";
             }
         }
     }
